9e7c736ba87f603f29f36a9768fe0b4ee32af3fa
Melissa <mawilliamson10@gmail.com>
2022-02-10 22:46:15 +0000

    create SQL query to add a new property

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -161,9 +161,14 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const newPropertyDetails = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bedrooms, property.number_of_bathrooms, property.country, property.street, property.city, property.province, property.post_code];
+  return pool
+    .query(`
+  INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bedrooms, number_of_bathrooms, country, street, city, province, post_code)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *
+  `, newPropertyDetails)
+    .then(result => result.rows[0])
+    .catch(err => null);
 }
 exports.addProperty = addProperty;
